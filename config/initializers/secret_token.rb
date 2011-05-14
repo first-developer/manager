bc5c4a700d5d476bac674cdfb9be0c3ea8f97de1
fd <fd@fd.(none)>
2011-05-14 15:41:09 +0200

    create project and migrate scaffold Pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manager::Application.config.secret_token = '6726505cfff8d232c06e9fa542443c6963daddae3b8f4153302a2950ce289fcc01bdfcf3335fa0dedabf69d1ce1d53c4e3f7eabf6edb4dd52ae8f269eb777516'
